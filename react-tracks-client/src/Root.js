854a2d2eff98c99e851a296eced060fb99f97650
Ali Khalil <ali_khalil@hotmail.co.uk>
2019-09-16 18:07:51 +0100

    added in register form

--- react-tracks-client/src/Root.js
+++ react-tracks-client/src/Root.js
@@ -2,6 +2,7 @@ import React from "react";
 import withRoot from "./withRoot";
 import { Query } from "react-apollo";
 import { gql } from "apollo-boost";
+import Auth from './components/Auth'
 
 const GET_TRACKS = gql`
   {
@@ -15,13 +16,14 @@ const GET_TRACKS = gql`
 `;
 
 const Root = () => (
-  <Query query={GET_TRACKS}>
-    {({ data, loading, error }) => {
-      if (loading) return <div> loading.. </div>;
-      if (error) return <div> error: {error} </div>;
-      return <div> data: {JSON.stringify(data, null, 2)} </div>;
-    }}
-  </Query>
+  <Auth/>
+  // <Query query={GET_TRACKS}>
+  //   {({ data, loading, error }) => {
+  //     if (loading) return <div> loading.. </div>;
+  //     if (error) return <div> error: {error} </div>;
+  //     return <div> data: {JSON.stringify(data, null, 2)} </div>;
+  //   }}
+  // </Query>
 );
 
 export default withRoot(Root);
--- react-tracks-client/src/components/Auth/Register.js
+++ react-tracks-client/src/components/Auth/Register.js
@@ -1,23 +1,101 @@
 import React from "react";
+import { Mutation } from "react-apollo";
+import { gql } from "apollo-boost";
 import withStyles from "@material-ui/core/styles/withStyles";
-// import Typography from "@material-ui/core/Typography";
-// import Avatar from "@material-ui/core/Avatar";
-// import FormControl from "@material-ui/core/FormControl";
-// import Paper from "@material-ui/core/Paper";
-// import Input from "@material-ui/core/Input";
-// import InputLabel from "@material-ui/core/InputLabel";
-// import Button from "@material-ui/core/Button";
-// import Dialog from "@material-ui/core/Dialog";
-// import DialogActions from "@material-ui/core/DialogActions";
-// import DialogContent from "@material-ui/core/DialogContent";
-// import DialogContentText from "@material-ui/core/DialogContentText";
-// import DialogTitle from "@material-ui/core/DialogTitle";
-// import Slide from "@material-ui/core/Slide";
-// import Gavel from "@material-ui/icons/Gavel";
-// import VerifiedUserTwoTone from "@material-ui/icons/VerifiedUserTwoTone";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+import FormControl from "@material-ui/core/FormControl";
+import Paper from "@material-ui/core/Paper";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Slide from "@material-ui/core/Slide";
+import Gavel from "@material-ui/icons/Gavel";
+import VerifiedUserTwoTone from "@material-ui/icons/VerifiedUserTwoTone";
 
 const Register = ({ classes }) => {
-  return <div>Register</div>;
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <Gavel />
+        </Avatar>
+        <Typography variant="headline">Register</Typography>
+
+
+      {/* <Mutation>
+        {
+         () => { */}
+
+            {/* return ( */}
+              <form
+              // onSubmit={event => handleSubmit(event, createUser)}
+              className={classes.form}
+            >
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="username">Username</InputLabel>
+                <Input
+                  id="username"
+                  // onChange={event => setUsername(event.target.value)}
+                />
+              </FormControl>
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="email">Email</InputLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  // onChange={event => setEmail(event.target.value)}
+                />
+              </FormControl>
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="password">Password</InputLabel>
+                <Input
+                  id="password"
+                  type="password"
+                  // onChange={event => setPassword(event.target.value)}
+                />
+              </FormControl>
+
+              <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="secondary"
+                  // disabled={
+                  //   loading ||
+                  //   !username.trim() ||
+                  //   !email.trim() ||
+                  //   !password.trim()
+                  // }
+                  className={classes.submit}
+                >
+                  {/* {loading ? "Registering..." : "Register"} */}
+                </Button>
+                <Button
+                  // onClick={() => setNewUser(false)}
+                  color="primary"
+                  variant="outlined"
+                  fullWidth
+                >
+                  Previous user? Log in here
+                </Button>
+
+              </form>
+            {/* ) */}
+         {/* } */}
+        {/* } */}
+
+
+      {/* </Mutation> */}
+
+      </Paper>
+    </div>
+  );
 };
 
 const styles = theme => ({
